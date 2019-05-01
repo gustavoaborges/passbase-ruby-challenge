9a4d447a42ded293cebd8768f1bd237160a7d0f4
Gustavo Borges <gustavoaborges@gmail.com>
2019-05-01 01:53:52 -0300

    a couple of helper methods for home controller

--- bank/app/controllers/home_controller.rb
+++ bank/app/controllers/home_controller.rb
@@ -1,3 +1,19 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  helper_method :balance,
+                :transactions
+
+  def balances
+    @balances ||= begin
+      blcs = {}
+      current_user.currencies_in_balance.each do |cur|
+        blcs[cur] = current_user.balance(cur)
+      end
+      blcs
+    end
+  end
+
+  def transactions
+    current_user.transactions.page(params[:page] || 1)
+  end
 end
